5183fde159275cefd9586d0b717eb72739382cc9
hector arteaga <hjarte1@hotmail.com>
2017-09-21 00:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function happyHolidaysTo(string){
   var name = string
   return `Happy holidays, ${name}!`
 }
+function happyHolidayTo(string1, string2){
+  var holiday = string1
+  var name = string2
+  return `Happy ${holiday}, ${name}!`
+}
