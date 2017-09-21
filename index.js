cfea259251e98a33b16df420eca81233a297d8de
hector arteaga <hjarte1@hotmail.com>
2017-09-21 00:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-functions happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`
-}
+// functions happyHolidaysTo(name){
+//   return `Happy holidays, ${name}!`
+// }
